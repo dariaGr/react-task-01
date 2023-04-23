4326672089ea0ef29635a3c9b9737266fe973810
dariaGr <grb747@inbox.ru>
2023-04-24 00:02:08 +0200

    fixed useFetch

--- src/UseFetch.js
+++ src/UseFetch.js
@@ -1,53 +1,37 @@
-import { useEffect, useReducer } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export const useFetch = url => {
-  const paramObj = {
-    params: {
-      _limit: 3,
-    },
-  }
-  const refetch = paramObj => {
-    return `${url}?_limit=${paramObj.params._limit}`
-  }
+  const [data, setData] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-  const initialState = {
-    data: [],
-    error: false,
-    isLoading: false,
-  }
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'isLoading':
-        return { ...initialState, isLoading: action.payload }
-      case 'fetched':
-        return { ...initialState, data: action.payload }
-      case 'error':
-        return { ...initialState, error: action.payload }
-      default:
-        return state
+  const fetchData = async (url, options) => {
+    setIsLoading(true)
+    try {
+      const response = await axios.get(url, options)
+      setData(response.data)
+    } catch (error) {
+      setError(error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const refetch = options => {
+    fetchData(url, options)
+  }
 
   useEffect(() => {
-    if (!url) return
-    dispatch({ type: 'refetch', payload: true })
-
-    try {
-      dispatch({ type: 'isLoading', payload: true })
-
-      axios.get(refetch(paramObj)).then(response => {
-        dispatch({ type: 'fetched', payload: response.data })
-      })
-    } catch (error) {
-      dispatch({ type: 'error', payload: true })
-    }
+    fetchData(url)
   }, [url])
 
-  return state
+  return {
+    data,
+    isLoading,
+    error,
+    refetch,
+  }
 }
 
 export default useFetch
